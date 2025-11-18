b9ae98b1682686e30cb72309f7d4918248df30ff
Thiago Giannini Ramos <thiagogiannini@tecgraf.puc-rio.br>
2025-11-18 16:58:11 -0300

    MOSC-2788 / Novo Módulo do Confocos

--- app/Services/Confocos/DocumentoConselhoService.php
+++ app/Services/Confocos/DocumentoConselhoService.php
@@ -24,7 +24,11 @@ class DocumentoConselhoService
 
     public function getPorId($id)
     {
-        return $this->repo->get($id);
+        $documento = $this->repo->get($id);
+
+        $documento['arquivo'] = $this->getArquivoDocumento($documento);
+
+        return $documento;
     }
 
     public function getListaDocumentosPorConselho($id_conselho)
@@ -45,5 +49,39 @@ class DocumentoConselhoService
     public function destroy($id_conselho)
     {
         return $this->repo->destroy($id_conselho);
+    }
+
+    public function uploadDocumento($id_conselho, $file)
+    {
+        $filename = str_replace(" ", "-", $file->getClientOriginalName());
+
+        $filename = preg_replace("/[^A-Za-z0-9-.]/","", $filename);
+
+        $filenameRandom = rand(1,10000)."-".$id_conselho."-".$filename;
+
+        $this->successFile = Storage::putFileAs("/confocos/", $file, $filenameRandom);
+
+        if ( $this->successFile ) {
+
+            $data = [
+                'id_conselho' => $id_conselho,
+                'tx_titulo_documento' => $filenameRandom,
+                'tx_caminho_arquivo' => str_replace("//", "/", $this->successFile),
+                'tx_tipo_arquivo' => $file->getClientMimeType(),
+                'dt_data_cadastro' => date('Y-m-d H:i:s'),
+            ];
+
+            return $this->repo->store($data);
+        }
+
+
+        return 'data:image/png;base64,'.base64_encode(file_get_contents(storage_path('app/confocos/'.$filenameRandom)));
+    }
+
+    public function getArquivoDocumento($documento) {
+
+        $arquivo = base64_encode(file_get_contents(storage_path('app/'. $documento->tx_caminho_arquivo)));
+
+        return $arquivo;
     }
 }
